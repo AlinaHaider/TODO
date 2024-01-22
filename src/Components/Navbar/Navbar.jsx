2c179f83b4192d4c5a1ed799d10980feda79a58c
AlinaHaider <231497669@formanite.fccollege.edu.pk>
2024-01-22 13:47:19 +0500

    Second Commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,20 @@
 import {Link, NavLink} from "react-router-dom"
+import { useState } from "react"
 import './navbarstyle.css'
 
 export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
   return (
     <nav className="nav">
         <Link to="/" className="home">Home</Link>
-        <ul className="ul">
+        <div className="menu" onClick={ () => {
+          setMenuOpen(!menuOpen)
+        }}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <ul className={menuOpen ? "open" : ""}>
             <li>
                 <NavLink to="/about">About</NavLink>
             </li>
